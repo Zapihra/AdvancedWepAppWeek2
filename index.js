da0b043f84693c2254c0c244e9fb17004ef5154d
Zapihra <iida.vaaja@student.lut.fi>
2023-11-10 19:33:41 +0200

    task3 post /sum

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.json())
 
 app.get('/hello', (req,res) => {
     res.send({
@@ -10,6 +11,18 @@ app.get('/hello', (req,res) => {
 
 app.get('/echo/:id', (req,res) => {
     res.send(req.params);
+})
+
+app.post('/sum', (req, res) => {
+    
+    const numb = req.body.numbers;
+    let sum = 0;
+
+    for (let i = 0; i < numb.length; i++) {
+        sum += numb[i];
+    }
+
+    res.json({"sum": sum})
 })
 
 app.listen(port, () => {
